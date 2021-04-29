846c722301de8afd67417ad34a0cee10479e27b7
millerh1 <millerh1@livemail.uthscsa.edu>
2021-04-28 19:30:29 -0500

    Fixed the recipe & chmod issue

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -7,6 +7,7 @@ RSEQ_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $RSEQ_HOME
 cp -R $SRC_DIR/* $RSEQ_HOME/
 
-# Create symbolic links for a5_miseq's launch script
+# Create symbolic links for RSeq launch script
 mkdir -p $BINARY_HOME
+chmod +x $RSEQ_HOME/bin/RSeq
 ln -s $RSEQ_HOME/bin/RSeq $BINARY_HOME/
